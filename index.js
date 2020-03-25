5dbc543489f6adf65cf74af759a1fb7a4dfabdb7
Vijayalakshmi-krishna <61419132+Vijayalakshmi-krishna@users.noreply.github.com>
2020-03-25 22:41:54 +0530

    Updated

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const bodyParser=require('body-parser')
+const cors=require('cors');
 const app=express();
+app.use(cors());
 var myData=[];
-//app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); 
 app.get('/',function (req,res){
 res.json(myData)
 });
@@ -14,16 +17,16 @@ app.post("/users",function(req,res){
     res.json({message:"Request post received"});
 })
 
-app.put("/users/:id",function(req,res){
+// app.put("/users/:id",function(req,res){
 
-console.log(req.params.id);
-console.log(req.body);
-myData.push(req.body);
-res.json({message:"Request updated"});
-})
-app.delete("/users/:id",function(req,res){
-    res.json({message:"Deleted"})
-})
+// console.log(req.params.id);
+// console.log(req.body);
+// myData.push(req.body);
+// res.json({message:"Request updated"});
+// })
+// app.delete("/users/:id",function(req,res){
+//     res.json({message:"Deleted"})
+// })
 app.listen(3000,function (){
     console.log("Port is running in 3000...")
 });

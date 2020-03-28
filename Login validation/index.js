06cbb1a895934b341103f542737fa7f976c8e955
Vijayalakshmi-krishna <61419132+Vijayalakshmi-krishna@users.noreply.github.com>
2020-03-29 00:25:22 +0530

    Add files via upload

--- Login validation/index.js
+++ Login validation/index.js
@@ -6,11 +6,30 @@ const mongoClient=require('mongodb');
 const url="mongodb://localhost:27017"
 const bcrypt=require('bcrypt');
 const saltrounds=10;
-
-
+const jwt=require('jsonwebtoken');
+//var LocalStorage = require('node-localstorage').LocalStorage,
+//localStorage = new LocalStorage('./scratch');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+function authenticate(req,res,next)
+{
+    let header=req.header('Authorization')
+    console.log(header)
+    if(header ==undefined)
+    {
+        res.status(401).json({
+            message:"unauthorized"
+        });
+    }
+    else{
+       var decode= jwt.verify(header,'abcghimno');
+       console.log(decode);
+       next();
+    }
+    
+}
 
 app.post("/register",function(req,res){
    console.log(req.body);
@@ -27,7 +46,7 @@ app.post("/register",function(req,res){
            bcrypt.hash(req.body.password,salt,function(err,hash){
             if(err) throw err;
             console.log(hash);
-            newData.password=hash;
+            newData.password=hash;  
 
             db.collection("userlogin").insertOne(newData,function(err,result){
                 if(err) throw err;
@@ -56,14 +75,32 @@ app.post("/login",function(req,res){
         db.collection("userlogin").findOne({email:req.body.email},function(err,userData){
             if(err) throw err;
             console.log(userData);
-        bcrypt.compare(req.body.password,userData.password,function(err,result){
+            bcrypt.compare(req.body.password,userData.password,function(err,result){
             console.log(result);
             if(result)
             {
-                res.json({message:"success"})
+                var jwtToken=jwt.sign({id:userData.id},'abcghimno')
+                console.log(jwtToken);
+               // localStorage.setItem("token",jwtToken);
+                res.json({message:"success",
+                token:jwtToken})
+            }
+            else
+            {
+                res.json({message:"Login Failed",
+               })
             }
         })
         })
+    })
+})
+
+
+app.get("/dashboard",authenticate,function(req,res)
+{
+    console.log("in dashboard")
+    res.json({
+        message:"protected"
     })
 })
 

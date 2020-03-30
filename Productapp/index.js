488a40c1f6253ca5410f11253dcb0db7a27b99be
Vijayalakshmi-krishna <61419132+Vijayalakshmi-krishna@users.noreply.github.com>
2020-03-30 08:05:09 +0530

    Added

---
+++ Productapp/index.js
@@ -0,0 +1,204 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoClient = require('mongodb');
+const url = "mongodb://localhost:27017"
+const bcrypt = require('bcrypt');
+const saltrounds = 10;
+const jwt = require('jsonwebtoken');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//function to authenticate the user with valid JWT token
+function authenticate(req, res, next) {
+    let header = req.header('Authorization')
+
+    if (header == undefined) {
+        res.status(401).json({
+            message: "unauthorized"
+        });
+    }
+    else {
+        //Allow users with valid token
+        var decode = jwt.verify(header, 'abcghimno');
+        next();
+    }
+
+}
+//New users Route
+app.post("/register", function (req, res) {
+    mongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+        if (err) throw err;
+        var db = client.db("appdb");
+        var newData = {
+            email: req.body.email
+        }
+        //generate salt
+        bcrypt.genSalt(saltrounds, function (err, salt) {
+            if (err) throw err;
+            //encrypt the password
+            bcrypt.hash(req.body.password, salt, function (err, hash) {
+                if (err) throw err;
+                newData.password = hash;
+                //Insert new user into DB
+                db.collection("userdetails").insertOne(newData, function (err, result) {
+                    if (err) throw err;
+                    client.close();
+                    res.json({
+                        message: "User registered"
+                    })
+                })
+            })
+        })
+
+
+
+    })
+
+
+})
+
+//Already registered users Route
+app.post("/login", function (req, res) {
+
+    mongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+        if (err) throw err;
+        var db = client.db("appdb");
+        db.collection("userdetails").findOne({ email: req.body.email }, function (err, userData) {
+            if (err) throw err;
+
+            //compare the password and generate jwt token
+            bcrypt.compare(req.body.password, userData.password, function (err, result) {
+
+                if (result) {
+                    var jwtToken = jwt.sign({ id: userData.id }, 'abcghimno')
+                    res.json({
+                        message: "success",
+                        token: jwtToken
+                    })
+                }
+                else {
+                    res.json({
+                        message: "Login Failed",
+                    })
+                }
+            })
+        })
+    })
+})
+
+//Logged in users Route
+app.get("/dashboard", authenticate, function (req, res) {
+    //console.log(req.body.json());
+    res.json({
+        message: "protected"
+    })
+})
+
+app.get('/user', function (req, res) {
+    
+    mongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db("appdb");
+        var userData = db.collection("userdetails").findOne({email:localStorage.getItem("email")}).toArray();
+
+        userData.then(function (data) {
+            console.log(data);
+            console.log("Data Displayed");
+            client.close();
+            
+            res.json(data)
+
+        })
+            .catch(function (err) {
+                client.close();
+                res.json({
+                    message: "error"
+                })
+            })
+    });
+});
+
+app.post("/insert", function (req, res) {
+    mongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+        if (err) throw err;
+        var db = client.db("appdb");
+        var newData = {
+            product: req.body.product,
+            quantity: req.body.quantity
+        }
+        db.collection("UserPdt").insertOne(newData, function (err, result) {
+            if (err) throw err;
+            client.close();
+            res.json({
+                messsage: "product added"
+            })
+        })
+    })
+});
+
+app.get("/display", function (req, res) {
+    mongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db("appdb");
+        var dispcart = db.collection("UserPdt").find().toArray();
+        dispcart.then(function (data) {
+            console.log(data);
+            console.log("Data Displayed");
+            client.close();
+            res.json(data);
+        })
+            .catch(function (err) {
+                client.close();
+                res.json({
+                    message: "error"
+                })
+            })
+    });
+});
+
+app.delete("/delete", function (req, res) {
+
+    console.log(req.body);
+    mongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db("appdb");
+        db.collection("UserPdt").deleteOne({ product: req.body.product }, function (err, result) {
+            if (err) throw err;
+            console.log("deleted in DB");
+            client.close();
+            res.json({
+                message: " product Deleted"
+            })
+        });
+    });
+});
+
+app.put("/update", function (req, res) {
+
+    console.log(req.body);
+    mongoClient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db("appdb");
+        db.collection("UserPdt").updateOne(
+            { product: req.body.product },
+            { $set: { quantity: req.body.quantity } }, function (err, result) {
+                if (err) throw err;
+                console.log("updated to db");
+                client.close();
+                res.json({
+                    message: "Product updated"
+                })
+            });
+
+    });
+});
+
+
+
+app.listen(3000, function () {
+    console.log("Port is running in 3000...")
+})

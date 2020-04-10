03b43949cc3978d26e0aead41364f401177eea44
Vijayalakshmi-krishna <61419132+Vijayalakshmi-krishna@users.noreply.github.com>
2020-04-10 07:28:13 +0530

    Updated

--- Angular/pricingtable/app/app.module.ts
+++ Angular/pricingtable/app/app.module.ts
@@ -2,18 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { StarterComponent } from './starter/starter.component';
-import { ProfessionalComponent } from './professional/professional.component';
-import { BusinessComponent } from './business/business.component';
-import { EnterpriseComponent } from './enterprise/enterprise.component';
+import { PricecardComponent } from './pricecard/pricecard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    StarterComponent,
-    ProfessionalComponent,
-    BusinessComponent,
-    EnterpriseComponent
+    PricecardComponent,
+   
   ],
   imports: [
     BrowserModule
